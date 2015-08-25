33fb13b7b24a7a12f766e232de8b3f9ce26eb7c5
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2015-08-24 20:55:29 -0400

    Corrected the function and var names

--- src/morse_encoder/core.clj
+++ src/morse_encoder/core.clj
@@ -7,15 +7,18 @@
                \G "--." \H "..." \I ".." \J ".---" \K "-.-" \L ".-.."
                \M "--" \N "-." \O "---" \P ".--." \Q "--.-" \R ".-."
                \S "..." \T "-" \U "..-" \V "...-" \W ".--" \X "-..-"
-               \Y "-.--" \Z "--.." \space "/"})
+               \Y "-.--" \Z "--.." \space "/" \0 "-----" \1 ".----"
+               \2 "..---" \3 "...--" \4 "....-" \5 "....." \6 "-...."
+               \7 "--..." \8 "-...." \9 "----." \. ".-.-.-" \, "--..--"
+               \: "---..." \? "..--.." \' ".----." \- "-....-" })
 
-(def dit (sinusoid 0.2 440))
-(def dah (sinusoid 0.6 440))
-(def signal-space (silence 0.2 1))
-(def letter-space (silence 0.6 1))
-(def word-space (silence 0.8 1))
+(def dit (sinusoid 0.1 440))
+(def dah (sinusoid 0.3 440))
+(def signal-gap (silence 0.1 1))
+(def short-gap (silence 0.3 1))
+(def medium-gap (silence 0.6 1))
 
-(defn get-symbol-sound
+(defn get-signal-symbol
   [symbol]
   (case symbol
     \. dit
@@ -28,33 +31,31 @@
        (char-array)
        (seq)))
 
-(defn morse
+(defn to-morse
   [message]
   (let [characters (break-to-chars message)]
     (map #(alphabet %) characters)))
 
-(defn build-letter-sound
+(defn build-letter-sound-signals
   [morse-letter]
   (let [symbols (break-to-chars morse-letter)
         sounds (map get-symbol-sound symbols)]
     (if (> (count sounds) 1)
-      (reduce #(append (append %1 signal-space) %2) sounds)
-      (append (first sounds) signal-space))))
+      (reduce #(append (append %1 signal-gap) %2) sounds)
+      (first sounds))))
 
-(def build-letter-sound-memoized (memoize build-letter-sound))
-
-(defn build-words-sounds
+(defn build-words-sound-signals
   [morse-letters]
-  (map build-letter-sound-memoized morse-letters))
+  (map memoized morse-letters))
 
-(defn append-sounds
+(defn append-sound-signals
   [sounds]
-  (reduce #(append %1 (append letter-space %2)) sounds))
+  (reduce #(append %1 (append short-gap %2)) sounds))
 
 
-(defn encode-to-morse
+(defn to-morse-signals
   [message]
-  (-> (morse message)
-      (build-words-sounds)
-      (append-sounds)
+  (-> (to-morse message)
+      (build-words-sound-signals)
+      (append-sound-signals)
       (#(save %1 "sample.wav" 44100))))
